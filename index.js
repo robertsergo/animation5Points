be1db125cf545f01bdcaed3e11dcf774b5deed06
robertsergo <robertsergodimanche@gmail.com>
2023-02-22 09:28:04 -0500

    index css

---
+++ index.js
@@ -0,0 +1,19 @@
+let imageBG = document.getElementById("bg");
+let container = document.getElementById("container");
+let imageBGBounds = imageBG.getBoundingClientRect();
+let circle = document.getElementById("circle")
+let circleBounds = circle.getBoundingClientRect();
+window.onload = function() { sizeImage() }
+window.addEventListener("resize", () => { sizeImage() });
+  
+function sizeImage() {
+  let imageBGBounds = imageBG.getBoundingClientRect();
+  let circleBounds = circle.getBoundingClientRect();
+  let ratio = 320/853;
+  let width = window.innerWidth;
+  let height = width * ratio;
+  imageBG.style.width = width+'px';
+  container.style.width = width+'px';
+  imageBG.style.height = height+"px";
+  container.style.height = height+"px";
+};
